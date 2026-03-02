b1f9e4e568a30fc6b30b0ee72806bcc8452d65c1
Chinasa <kamogelomba@gmail.com>
2026-03-02 15:51:14 +0200

    made arrays

--- server/index.js
+++ server/index.js
@@ -5,4 +5,26 @@ import { parseString } from "xml2js";
 const app = express()
 const port = 3000
 
-app.listen()
+app.listen(port, ()=>{
+      console.log(`App running on port ${port}`)
+})
+
+
+
+/*
+Step 1:Turn string into an array
+SteP 2: Iterate over array and compare
+      go over array: check first over billing/accoun/tech array, whenever a word is found updata tally 
+
+
+*/
+
+let str = "I am trying to log into the amy account but keep getting the reply that my username does not exist, even tho I a registered before ";
+const billingArr =[]
+
+const accountArr =[]
+
+const technicalArr =[]
+function queryType (query){
+
+}
